d339a9f14edffbe8fa704f7abf5cdcad649902e2
Lahari Kantamaneni <110590529+ksatya2003@users.noreply.github.com>
2025-06-03 14:10:33 +0530

    Create GlobalExceptionHandler.java

---
+++ src/main/java/com/example/studentmanagement/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.example.studentmanagement.exception;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public String handleError(Exception e, Model model) {
+        model.addAttribute("errorMessage", e.getMessage());
+        return "error";
+    }
+}
